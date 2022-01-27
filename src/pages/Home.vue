ac77381cdd973ef250816a3e061979a7497f15b1
lxb <lxb007981@hotmail.com>
2022-01-27 18:49:17 +0800

    refactor: introduce windicss

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,11 +1,14 @@
 <template>
-  <div id="homeContainer">
-    <h1>Trilemma Generator</h1>
-    <el-input v-model="title" placeholder="Title" />
-    <el-input v-model="item1" placeholder="Item 1" />
-    <el-input v-model="item2" placeholder="Item 2" />
-    <el-input v-model="item3" placeholder="Item 3" />
-    <el-button type="primary" round size="large" @click="generate">Generate</el-button>
+  <div class="py-6 min-h-screen flex flex-col items-center bg-gray-50 justify-center">
+    <h1 class="font-bold text-6xl my-7 text-center">Trilemma Generator</h1>
+    <input v-model="title" class="input" placeholder="Title" />
+    <input v-model="item1" class="input" placeholder="Item 1" />
+    <input v-model="item2" class="input" placeholder="Item 2" />
+    <input v-model="item3" class="input" placeholder="Item 3" />
+    <button
+      class="px-6 py-6 text-5xl text-blue-500 font-semibold rounded-full border my-7 min-w-fit border-blue-200 hover:text-white hover:bg-blue-500 hover:border-transparent focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2"
+      @click="generate"
+    >Generate</button>
   </div>
 </template>
 <script lang="ts" setup>
@@ -15,41 +18,13 @@ const item1 = ref('')
 const item2 = ref('')
 const item3 = ref('')
 const title = ref('')
+
 function generate() {
   router.push({ path: '/trilemma', query: { title: title.value, item1: item1.value, item2: item2.value, item3: item3.value } })
 }
 </script>
 <style lang="css" scoped>
-h1 {
-  text-align: center;
-  font-size: 4em;
-  margin-bottom: 0.5em;
-}
-#homeContainer {
-  margin: 1em auto;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-}
-:deep(.el-input__inner) {
-  border: 2px solid #aaa;
-  margin: 20px;
-  font-size: 3.8em;
-  height: 1.6em;
-  text-align: center;
-  color: black;
-}
-:deep(.el-input__inner:focus) {
-  border-color: cornflowerblue;
-  box-shadow: 0 4px 10px rgb(0 0 0 / 10%);
-}
-
-:deep(.el-input) {
-  width: 50%;
-}
-
-:deep(.el-button) {
-  font-size: 3em;
-  margin-top: 1em;
+.input {
+  @apply placeholder:italic placeholder:text-slate-400 block m-4 min-w-fit w-1/3 h-21 px-3 py-2 bg-white border border-slate-300 rounded-md text-4xl shadow-sm text-center placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-1 focus:ring-sky-500;
 }
 </style>
--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import vue from "@vitejs/plugin-vue";
 
 import Components from "unplugin-vue-components/vite";
 import { ElementPlusResolver } from "unplugin-vue-components/resolvers";
+import WindiCSS from 'vite-plugin-windicss'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -21,6 +22,7 @@ export default defineConfig({
   },
   plugins: [
     vue(),
+    WindiCSS(),
     Components({
       resolvers: [
         ElementPlusResolver({
